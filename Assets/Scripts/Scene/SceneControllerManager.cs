533454248c40800b99cfde099761f602fe20ee8c
cattail <334750034@qq.com>
2023-03-02 22:28:36 +0800

    修改了部分bug，现在幕间图像不再遮挡其他ui

---
+++ Assets/Scripts/Scene/SceneControllerManager.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+///<summary>
+/// 定义场景控制器类
+///</summary>
+public class SceneControllerManager : MonoBehaviour
+{
+	
+}
