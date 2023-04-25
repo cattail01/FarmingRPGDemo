7be41af9a0473bb285bc590d9cdd7af9cc324d93
cattail <334750034@qq.com>
2023-04-25 13:05:37 +0800

    开发农场功能核心类

---
+++ Assets/Scripts/Crop/CropDetails.cs
@@ -0,0 +1,55 @@
+﻿using Enums;
+using System;
+using UnityEngine;
+
+
+[Serializable]
+public class CropDetails
+{
+    [ItemCodeDescription] public int SeedItemCode;
+    public int[] GrowthDays;
+    public int TotalGrowthDays;
+    public GameObject[] GrowthPrefab;
+    public Sprite[] GrowthSprites;
+    public Season[] Seasons;
+    public Sprite HarvestedSprite;
+    [ItemCodeDescription] public int HarvestedTransformItemCode;
+    public bool HideCropBeforeHarvestedAnimation;
+    public bool DisableCropCollidersBeforeHarvestedAnimation;
+    public bool IsHarvestedAnimation;
+    public bool IsHarvestActionEffect = false;
+    public bool SpawnCropProducedAtPlayerPosition;
+    public HarvestActionEffect HarvestActionEffect;
+
+    [ItemCodeDescription] public int[] HarvestToolItemCodes;
+    public int[] RequiredHarvestActions;
+    [ItemCodeDescription] public int[] CropProducedItemCode;
+    public int[] CropProducedMinQuantity;
+    public int[] CropProducedMaxQuantity;
+    public int DaysToRegrow;
+
+    public bool CanUseToolToHarvestCrop(int toolItemCode)
+    {
+        if (RequiredHarvestActionsForTool(toolItemCode) == -1)
+        {
+            return false;
+        }
+        else
+        {
+            return true;
+        }
+    }
+
+    public int RequiredHarvestActionsForTool(int toolItemCode)
+    {
+        for (int i = 0; i < HarvestToolItemCodes.Length; ++i)
+        {
+            if (HarvestToolItemCodes[i] == toolItemCode)
+            {
+                return RequiredHarvestActions[i];
+            }
+        }
+
+        return -1;
+    }
+}
---
+++ Assets/Scripts/Crop/SO_CropDetailsList.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "CropDetailsList", menuName = "ScriptableObject/Crop/CropDetailsList")]
+public class SO_CropDetailsList : MonoBehaviour
+{
+    [SerializeField]
+    public List<CropDetails> CropDetails;
+
+    public CropDetails GetCropDetails(int seedItemCode)
+    {
+        return CropDetails.Find(x => x.SeedItemCode == seedItemCode);
+    }
+}
