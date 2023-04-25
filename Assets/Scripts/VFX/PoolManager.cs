82e4620ce8df487006b79f869729a4fab79d1526
cattail <334750034@qq.com>
2023-04-25 08:34:22 +0800

    开始开发对象池

---
+++ Assets/Scripts/VFX/PoolManager.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PoolManager : MonoBehaviour
+{
+    [System.Serializable]
+    public struct Pool
+    {
+        public int poolSize;
+        public GameObject prefab;
+    }
+
+    [SerializeField] private Pool[] pool = null;
+    [SerializeField] private Transform objectPoolTransform = null;
+
+    private Dictionary<int, Queue<GameObject>> poolDictionary = new Dictionary<int, Queue<GameObject>>();
+
+    private void Start()
+    {
+        for (int i = 0; i < pool.Length; ++i)
+        {
+            CreatePool(pool[i].prefab, pool[i].poolSize);
+        }
+    }
+
+    private void CreatePool(GameObject prefab, int poolSize)
+    {
+        int poolKey = prefab.GetInstanceID();
+        string prefabName = prefab.name;
+
+        // 创建父游戏物体
+        // 用于保存创建的多个预制件实例
+        GameObject parentGameObject = new GameObject($"{prefabName}Anchor");
+
+        parentGameObject.transform.SetParent(objectPoolTransform);
+
+        if (!poolDictionary.ContainsKey(poolKey))
+        {
+            poolDictionary.Add(poolKey, new Queue<GameObject>());
+
+            for (int i = 0; i < poolSize; ++i)
+            {
+                GameObject newObject = Instantiate(prefab, parentGameObject.transform) as GameObject;
+                newObject.SetActive(false);
+
+                poolDictionary[poolKey].Enqueue(newObject);
+            }
+        }
+    }
+
+    public GameObject ReuseObject(GameObject prefab, Vector3 position, Quaternion rotation)
+    {
+        int poolKey = prefab.GetInstanceID();
+
+        if (poolDictionary.ContainsKey(poolKey))
+        {
+            GameObject objectToReuse = GetObjectFromPool(poolKey);
+
+            
+        }
+    }
+
+    public GameObject GetObjectFromPool(int poolKey)
+    {
+        throw new NotImplementedException();
+    }
+}
